ade2a2cb7446e1af940871e2b97d010aa118a5b3
s-kenza <kenza.schuler@gmail.com>
2025-04-30 11:28:49 +0200

    feat: :sparkles: refactor TypeOrmModule configuration to use external typeOrmConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { typeOrmConfig } from './config/typeorm.config'; // Assurez-vous que le chemin est correct
 
 @Module({
   imports: [
     ConfigModule.forRoot(), // Charge les variables d'env depuis .env
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: Number(process.env.DATABASE_PORT) || 3306,
-      username: process.env.DATABASE_USER || 'root',
-      password: process.env.DATABASE_PASSWORD || '',
-      database: process.env.DATABASE_NAME || 'qvema',
-      autoLoadEntities: true, // Charge automatiquement les entités
-      synchronize: false, // Synchronisation automatique en dev (désactiver en prod)
-    }), 
+    TypeOrmModule.forRoot(
+     typeOrmConfig), 
     UsersModule,
     AuthModule,
   ]
